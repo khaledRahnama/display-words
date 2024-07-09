fd24d089f57bd2da8765d546a2cde84db0313eae
khaledRahnama <157706646+khaledRahnama@users.noreply.github.com>
2024-07-09 20:43:29 +0300

    words come from top

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ const textContainer = document.getElementById('text-container');
 const backgroundMusic = document.getElementById('background-music');
 const words = text.split(' ');
 let currentIndex = 0;
-let currentText = '';
 
 backgroundMusic.play();
 
 function displayNextWord() {
     if (currentIndex < words.length) {
-        currentText += words[currentIndex] + ' ';
-        textContainer.style.width = `${textContainer.clientWidth + words[currentIndex].length * 10}px`;
-        textContainer.textContent = currentText;
+        const wordElement = document.createElement('span');
+        wordElement.textContent = words[currentIndex] + ' ';
+        wordElement.className = 'word';
+        textContainer.appendChild(wordElement);
         currentIndex++;
         setTimeout(displayNextWord, 500); // Adjust the delay to control the speed of text display
     }
